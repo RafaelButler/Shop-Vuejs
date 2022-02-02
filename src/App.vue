d1607f54fa94c2f4dcd9fb2e7614c97c20892a51
RafaelButler <rafaelbutler80@gmail.com>
2022-02-02 16:22:02 -0300

    color pallet

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-import './styles/global.css';
+import "./styles/global.css";
+import Header from "./components/Header.vue";
 </script>
 
 <template>
-  <router-view></router-view>
+  <Header />
+  <div class="main">
+    <router-view></router-view>
+  </div>
 </template>
 
-<style>
-
-</style>
+<style></style>
