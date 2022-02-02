8cbd68c808db485981518a2531aff9afb00f8b29
RafaelButler <rafaelbutler80@gmail.com>
2022-02-02 19:13:06 -0300

    Product card

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: "ProductCard",
+};
+</script>
+
+<template>
+  <div class="card" style="width: 18rem">
+    <img
+      src="https://chicorei.imgix.net/21351/bd5f7dd0-74b4-11ec-b94f-454db13126f7.jpg?auto=compress,format&q=65&w=425&h=600&fit=crop&crop=top"
+      class="card-img-top"
+      alt="..."
+    />
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </p>
+      <p>R$ 19,90</p>
+    </div>
+  </div>
+</template>
--- src/views/pages/Home.vue
+++ src/views/pages/Home.vue
@@ -1,6 +1,43 @@
-<script></script>
+<script>
+import ProductCard from "../../components/ProductCard.vue";
+
+export default {
+  components: {
+    ProductCard,
+  },
+};
+</script>
 
 <template>
   Hello Home Component
   <router-link to="/about">Go to about page</router-link>
+
+  <div class="container mt-4">
+    <div class="wrapperCards">
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+    </div>
+  </div>
 </template>
+
+<style lang="scss">
+.wrapperCards {
+  display: grid;
+  grid-template-columns: 100%;
+  gap: 2rem;
+  text-align: center;
+  justify-content: center;
+  justify-items: center;
+
+  @media (min-width: 641px) {
+    grid-template-columns: 50% 50%;
+    gap: 3rem;
+  }
+
+  @media (min-width: 700px) {
+    grid-template-columns: 33% 33% 33%;
+  }
+}
+</style>
